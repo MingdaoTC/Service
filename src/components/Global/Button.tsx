66dd46bc7d4844a9be23a8e82fc17d96f57f4eab
banahaker <pn0818x@outlook.com>
2024-07-25 20:21:17 +0800

    refactor: update Button component to include customStyle prop

--- src/components/Global/Button.tsx
+++ src/components/Global/Button.tsx
@@ -6,9 +6,16 @@ type TProps = {
   styleType: "primary" | "secondary" | "danger";
   href?: string;
   onClick?: () => void;
+  customStyle?: string;
 };
 
-export default function Button({ children, styleType, href, onClick }: TProps) {
+export default function Button({
+  children,
+  styleType,
+  href,
+  onClick,
+  customStyle,
+}: TProps) {
   const styles = {
     default: "px-4 py-2 rounded-md hover:rounded-sm transition-all",
     primary: "bg-mingdao-blue text-white",
@@ -18,12 +25,15 @@ export default function Button({ children, styleType, href, onClick }: TProps) {
   return (
     <>
       {href ? (
-        <Link href={href} className={`${styles[styleType]} ${styles.default}`}>
+        <Link
+          href={href}
+          className={`${styles[styleType]} ${styles.default} ${customStyle}`}
+        >
           {children}
         </Link>
       ) : (
         <button
-          className={`${styles[styleType]} ${styles.default}`}
+          className={`${styles[styleType]} ${styles.default} ${customStyle}`}
           onClick={onClick}
         >
           {children}
