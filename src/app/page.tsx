2c5fd4aac408ee69d0c90c93a7a91c709bfb7795
banahaker <pn0818x@outlook.com>
2024-07-25 20:21:33 +0800

    feat: Add SearchBar component in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeaderBar from "@/components/Global/HeaderBar";
+import SearchBar from "@/components/Home/SearchBar";
 
 export default function Home() {
   return (
     <>
       <HeaderBar></HeaderBar>
+      <SearchBar></SearchBar>
     </>
   );
 }
---
+++ src/components/Home/SearchBar.tsx
@@ -0,0 +1,9 @@
+import SearchBox from "../Global/SearchBox";
+
+export default function SearchBar() {
+  return (
+    <div className="bg-mingdao-blue-light h-72 px-4 py-10 flex justify-center items-start">
+      <SearchBox></SearchBox>
+    </div>
+  );
+}
