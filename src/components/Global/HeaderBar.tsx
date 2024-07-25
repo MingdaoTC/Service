a0c0cbaaf7b2dab8102c8b03aa3e9c5b08f1839f
banahaker <pn0818x@outlook.com>
2024-07-25 18:53:09 +0800

    feat: header bar styling

---
+++ src/components/Global/HeaderBar.tsx
@@ -0,0 +1,21 @@
+import Button from "./Button";
+
+export default function HeaderBar() {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between py-2 px-8 w-full h-16 border ">
+        <div className="logo">
+          <div className="bg-slate-500 h-8 w-20"></div>
+        </div>
+        <div className="links flex flex-row items-center gap-2">
+          <Button styleType="secondary" href="/login">
+            Sign In
+          </Button>
+          <Button styleType="primary" href="/signup">
+            Enterprise Sign Up
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
