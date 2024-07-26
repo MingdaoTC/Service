39e9217118c3eb0dec225e55a48bb446c531bc68
HACO8888 <haco.tw@gmail.com>
2024-07-26 12:33:41 +0800

    feat: haderbar logo

--- src/components/Global/HeaderBar.tsx
+++ src/components/Global/HeaderBar.tsx
@@ -1,11 +1,21 @@
-import Button from "./Button";
+// Components
+import Button from "@/components/Global/Button";
+
+// Third-Party Library
+import Image from "next/image";
 
 export default function HeaderBar() {
   return (
     <>
       <div className="flex flex-row items-center justify-between py-2 px-8 w-full h-16 border ">
         <div className="logo">
-          <div className="bg-slate-500 h-8 w-20"></div>
+          <Image
+            src="/images/logo.png"
+            width={200}
+            height={0}
+            alt="logo"
+            className=" translate-y-[0.25rem]"
+          />
         </div>
         <div className="links flex flex-row items-center gap-2">
           <Button type="secondary" href="/login">
