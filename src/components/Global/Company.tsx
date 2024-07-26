01d25b4816fc238ad82c655a8a26a8ad100019a1
banahaker <pn0818x@outlook.com>
2024-07-26 12:12:13 +0800

    feat: Add Company component to CompanyList

--- src/components/Global/Company.tsx
+++ src/components/Global/Company.tsx
@@ -1,12 +1,57 @@
+import Image from "next/image";
+import { BiMap } from "react-icons/bi";
+import { BiBuildings } from "react-icons/bi";
+
 type Props = {
   name: string;
   location: string;
   category: string;
-  tag: string[];
+  tags: string[];
+  logo: string;
 };
 
 export default function Company(props: Props) {
   return (
-    <div className="px-6 py-4 border bg-white rounded-xl w-[calc(50%-1rem)] max-w-[90dvw]"></div>
+    <div className=" border bg-white rounded-xl w-[calc(50%-1rem)] max-w-[90dvw]">
+      <div className="flex gap-4 items-center px-6 pt-4">
+        <div
+          className="w-20 h-20 rounded-md border p-1"
+          style={{
+            backgroundImage: `url(${props.logo})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <h2 className="text-xl text-mingdao-blue-dark font-bold">
+          {props.name}
+        </h2>
+      </div>
+      <div className="py-4 px-6 ">
+        <div className="flex gap-2 items-center">
+          <BiMap color="gray" size={"1.25em"}></BiMap>
+          <span>{props.location}</span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <BiBuildings color="gray" size={"1.25em"}></BiBuildings>
+          <span>{props.category}</span>
+        </div>
+        <div className="flex gap-2 py-2 flex-wrap">
+          {props.tags.map((tag) => {
+            return (
+              <span
+                className="border px-2     py-[2px] rounded-lg text-sm text-gray-500"
+                key={tag}
+              >
+                {tag}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-full text-center text-white bg-mingdao-blue-dark rounded-b-xl py-3 cursor-pointer">
+        查看更多工作機會 (43)
+      </div>
+    </div>
   );
 }
--- src/components/Home/CompanyList.tsx
+++ src/components/Home/CompanyList.tsx
@@ -4,6 +4,14 @@ import { BiBuildings } from "react-icons/bi";
 // Components
 import Company from "../Global/Company";
 
+const testCompanyData = {
+  name: "英屬維京群島商太古可口可樂(股)公司台灣分公司",
+  location: "桃園市桃園區",
+  category: "飲料製造業",
+  tags: ["其他客服人員", "國內業務", "國外業務"],
+  logo: "https://cdn.lazco.dev/cocacola.png",
+};
+
 export default function CompanyList() {
   return (
     <div className="px-6 py-4 shadow-lg bg-white rounded-xl w-[90dvw] my-12 mx-auto relative -top-28">
@@ -16,7 +24,14 @@ export default function CompanyList() {
         推薦公司
       </h1>
       <div className="flex flex-wrap gap-4 py-4">
-        {/* <Company {...testCompanyData}></Company> */}
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
+        <Company {...testCompanyData}></Company>
       </div>
     </div>
   );
