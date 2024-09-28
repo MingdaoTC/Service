1e786d017168e26ce35973cc108b819ee14e5e41
OnCloud <anonymousaaaa41414141@gmail.com>
2024-09-29 04:09:22 +0800

    feat: add Company Info component
    Add a new component called Info to the Company directory. This component displays information about a company, including its title, location, seniority, education, and salary. It also includes a button for saving the company. The component uses the joinClass utility function for styling.

---
+++ src/components/Company/Info.tsx
@@ -0,0 +1,41 @@
+import { BiBookmark } from "react-icons/bi";
+
+import { joinClass } from "@/modules/joinClass";
+import Button from "../Global/Button";
+
+export default function Info({
+  data,
+  className,
+}: {
+  data: {
+    title: string;
+    company: string;
+    location: string;
+    seniority: string;
+    education: string;
+    salary: string;
+  };
+  className?: string;
+}) {
+  return (
+    <div
+      className={joinClass(
+        "w-full h-40 shadow-lg px-12 flex justify-between items-center",
+        className,
+      )}
+    >
+      <h1 className="text-4xl text-mingdao-blue-dark font-extrabold">
+        {data.company}
+      </h1>
+      <div className="flex h-fit gap-6">
+        <Button
+          type="secondary"
+          className="flex items-center gap-2 text-[1.3rem]"
+        >
+          <BiBookmark className="translate-y-[1px]" />
+          儲存
+        </Button>
+      </div>
+    </div>
+  );
+}
