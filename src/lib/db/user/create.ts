076fb50be047e71f2444b52b647904b3a0918891
banahaker <pn0818x@outlook.com>
2025-02-22 21:41:11 +0800

    fix: prisma create function's data should be wrapped in a object with "data" key

--- src/lib/db/user/create.ts
+++ src/lib/db/user/create.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/lib/prisma";
 import { User } from "@/types/User";
 
 export async function createUser(newData: User) {
-  const user = await prisma.user.create(newData);
+  const user = await prisma.user.create({
+    data: newData,
+  });
   return user;
 }
