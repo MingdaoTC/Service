cc275b2311256290479278568cfa63164b7b0305
banahaker <pn0818x@outlook.com>
2025-02-23 14:54:25 +0800

    refactor(r2): remove try-catch in upload function

--- src/lib/r2/upload.ts
+++ src/lib/r2/upload.ts
@@ -2,17 +2,11 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 import { r2 } from "./client";
 
-export async function upload(file: File, bucket: string, key: string) {
+export function upload(file: File, bucket: string, key: string) {
   const params = {
     Bucket: bucket,
     Key: key,
     Body: file,
   };
-  try {
-    const response = await r2.send(new PutObjectCommand(params));
-    return response;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  return r2.send(new PutObjectCommand(params));
 }
