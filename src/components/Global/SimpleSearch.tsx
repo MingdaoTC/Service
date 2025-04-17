08fa57ee957b31c59fea46e9b548d8fee11deb0c
banahaker <pn0818x@outlook.com>
2025-04-17 12:30:25 +0800

    feat: redirect function for SimpleSearch component

--- src/components/Global/SimpleSearch.tsx
+++ src/components/Global/SimpleSearch.tsx
@@ -3,13 +3,21 @@
 import { useState } from "react";
 
 import Button from "./Button";
+import { useRouter } from "next/navigation";
 
-export default function SimpleSearch() {
-  const [keyword, setKeyword] = useState("");
-  const [location, setLocation] = useState("");
-  const [category, setCategory] = useState("");
+type TProps = {
+  searchText?: string;
+};
 
-  const handleSearch = () => {};
+export default function SimpleSearch(props: TProps) {
+  const [keyword, setKeyword] = useState(props.searchText || "");
+
+  const router = useRouter();
+
+  const handleSearch = () => {
+    console.log(keyword);
+    router.push(`/search?q=${keyword}`);
+  };
 
   return (
     <div className="flex flex-row items-start justify-start gap-2 bg-white border border-mingdao-blue rounded-xl p-2">
@@ -17,6 +25,8 @@ export default function SimpleSearch() {
         type="text"
         className=" text-gray-900 text-sm w-full p-2.5 outline-none border-r"
         placeholder="關鍵字 (例如: 軟體工程師)"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
       />
 
       <Button
