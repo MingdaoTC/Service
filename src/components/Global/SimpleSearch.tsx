8eb0eb187be947262ad57ead7adef76636e5dd15
banahaker <pn0818x@outlook.com>
2025-04-17 11:14:45 +0800

    feat: new component "SimpleSearch" for temporary using.

---
+++ src/components/Global/SimpleSearch.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+import Button from "./Button";
+
+export default function SimpleSearch() {
+  const [keyword, setKeyword] = useState("");
+  const [location, setLocation] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="flex flex-row items-start justify-start gap-2 bg-white border border-mingdao-blue rounded-xl p-2">
+      <input
+        type="text"
+        className=" text-gray-900 text-sm w-full p-2.5 outline-none border-r"
+        placeholder="關鍵字 (例如: 軟體工程師)"
+      />
+
+      <Button
+        onClick={handleSearch}
+        className="whitespace-nowrap rounded-lg px-10"
+      >
+        搜尋
+      </Button>
+    </div>
+  );
+}
