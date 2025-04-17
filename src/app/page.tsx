bb714252bbd07053b4fee762e04428c61c1b058c
banahaker <pn0818x@outlook.com>
2025-04-17 11:19:23 +0800

    feat: apply "SimpleSearch" component in Home page and Search Result Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import JobList from "@/components/Home/JobList";
 import CompanyList from "@/components/Home/CompanyList";
-import SearchBox from "@/components/Global/SearchBox";
+// import SearchBox from "@/components/Global/SearchBox";
+import SimpleSearch from "@/components/Global/SimpleSearch";
 
 const testCompanyData = {
   name: "英屬維京群島商太古可口可樂(股)公司台灣分公司",
@@ -26,9 +27,9 @@ export default function Home() {
   return (
     <>
       <div className="bg-mingdao-blue-light h-72 px-4 py-10 flex justify-center items-start">
-        <SearchBox />
+        <SimpleSearch />
       </div>
-      <div className="flex flex-col gap-8 w-[90dvw] my-12 mx-auto relative -top-28">
+      <div className="flex flex-col gap-8 w-[90dvw] my-8 mx-auto relative -top-28">
         <JobList data={new Array(7).fill(testJobData)} />
         <CompanyList data={new Array(5).fill(testCompanyData)} />
       </div>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 // components
 import Result from "@/components/Search/Results";
-import SearchBox from "@/components/Global/SearchBox";
+// import SearchBox from "@/components/Global/SearchBox";
+import SimpleSearch from "@/components/Global/SimpleSearch";
 
 const search = () => {
   return (
     <>
       <div className="px-4 py-10 flex justify-center items-start">
-        <SearchBox />
+        <SimpleSearch />
       </div>
       <Result />
     </>
