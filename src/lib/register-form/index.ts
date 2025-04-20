39602b8c07fc48f5e1937d89f9ac88fd7d618348
HACO <haco.tw@gmail.com>
2025-04-20 16:06:20 +0800

    feat: push data content

--- src/lib/register-form/index.ts
+++ src/lib/register-form/index.ts
@@ -38,30 +38,24 @@ export async function handleAlumniRegister(email: any, formDataIn: any) {
   const fileResults = [];
 
   for (const file of files) {
-    // 只有当文件是有效的 File 对象时才尝试上传
     if (isValidFile(file.file)) {
       try {
-        // 创建文件名
         const extension = file.file.name.split(".").pop() || "";
         const filename = `${email}_${file.type}_${Date.now()}.${extension}`;
-
-        // 上传文件
-        const result = await upload(file.file, filename, file.file.type);
-        console.log(`File ${file.type} upload result:`, result);
-        fileResults.push({ type: file.type, result });
+        // const result = await upload(file.file, filename, file.file.type);
+        const result = null;
+        fileResults.push({ type: file.type, result, filename });
       } catch (error) {
         console.error(`Error uploading file ${file.type}:`, error);
         fileResults.push({ type: file.type, result: null });
       }
     } else {
-      // 如果没有有效文件，就将该类型文件的结果设为 "false"
       fileResults.push({ type: file.type, result: "false" });
     }
   }
 
-  // 转换结果为所需的 fileData 格式
   fileData = fileResults.reduce((acc: any, item: any) => {
-    acc[item.type] = item.result || "false";
+    acc[item.type] = item.filename || "false";
     return acc;
   }, {});
 
