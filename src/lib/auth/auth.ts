9927f05a75fc8ec7ebc386b4a8ac54af95e406e6
HACO8888 <haco.tw@gmail.com>
2025-04-23 02:48:43 +0800

    fix: format code for consistency and improve readability in auth.ts

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -16,11 +16,11 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
     async session({ session, _token }) {
       const defaultData = {
-        username:`${session.user.email.split("@")[0]}_${session.user.email.split("@")[1]}`,
+        username: `${session.user.email.split("@")[0]}_${session.user.email.split("@")[1]}`,
         displayName: session.user.name,
         email: session.user.email,
         avatarUrl: session.user.image,
-      }
+      };
 
       const data = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/user`, {
         method: "POST",
@@ -28,15 +28,17 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           "Content-Type": "application/json",
         },
         body: JSON.stringify(defaultData),
-      })
+      });
       const user = (await data.json()).data;
 
       return { user: user };
     },
     authorized: async ({ auth }) => {
-      console.log("Auth:", auth);
       if (auth) {
-        if (auth.user.role === UserRole.ADMIN || auth.user.role === UserRole.SUPERADMIN) {
+        if (
+          auth.user.role === UserRole.ADMIN ||
+          auth.user.role === UserRole.SUPERADMIN
+        ) {
           return true;
         }
         return false;
