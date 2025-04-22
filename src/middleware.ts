bb145820a37dc6b3d32a205c520a7adad1dc7757
HACO <haco.tw@gmail.com>
2025-04-23 02:45:25 +0800

    feat: 移除不必要的中介軟體邏輯，簡化管理路由的處理

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,3 @@
-// // middleware.ts
-// import { NextResponse } from "next/server";
-// import { NextRequest } from "next/server";
-
-// export async function middleware(request: NextRequest) {
-//   const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
-
-//   if (isAdminRoute) {
-//     try {
-//       const userResponse = await fetch(`${request.nextUrl.origin}/api/check-admin`, {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         method: "GET",
-//       });
-
-//       const userData = await userResponse.json();
-//       console.log("User Data:", userData);
-
-//       if (!userData.isAdmin) {
-//         return NextResponse.rewrite(new URL("/not-found", request.url));
-//       }
-//     } catch (error) {
-//       console.error("Error checking admin status:", error);
-//       return NextResponse.rewrite(new URL("/not-found", request.url));
-//     }
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/admin", "/admin/:path*"],
-// };
-
 export { auth as middleware } from "@/lib/auth/auth";
 
 export const config = {
