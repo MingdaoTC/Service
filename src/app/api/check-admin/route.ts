ecc85106349f3efadd521fd0c6e974849a1ad531
HACO <haco.tw@gmail.com>
2025-04-22 17:57:24 +0800

    feat: update prisma schema to include new user roles and account status, enhance user profile model, and modify application and registration models with enums for better status management refactor: adjust check-admin API route to utilize new UserRole enum for role checks chore: add '@biomejs/biome' to onlyBuiltDependencies in pnpm workspace configuration

--- src/app/api/check-admin/route.ts
+++ src/app/api/check-admin/route.ts
@@ -2,33 +2,33 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/lib/auth/auth";
 import { findUser } from "@/lib/db/user/find";
-import { User } from "@/prisma/client";
+import { User, UserRole } from "@/prisma/client";
 
 export async function GET() {
   try {
     const session = await auth();
     const user: User = session?.user as User;
-    
+
     if (!session || !user) {
-      return NextResponse.json(
-        { error: "Unauthorized" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    
+
     const userData = await findUser({ email: user.email });
-    
+
     let isAdmin = true;
-    if (userData?.role === "admin" || userData?.role === "superadmin") {
+    if (
+      userData?.role === UserRole.ADMIN ||
+      userData?.role === UserRole.SUPER_ADMIN
+    ) {
       isAdmin = true;
     }
-    
+
     return NextResponse.json({ isAdmin });
   } catch (error) {
     console.error("Error checking admin status:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
