d109b7017d79919d4fc17d04078379beec37cdef
HACO8888 <haco.tw@gmail.com>
2025-04-24 00:00:52 +0800

    feat: refactor middleware to improve session handling and user role validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,24 @@
 import { auth } from "@/lib/auth/auth";
 import { User, UserRole } from "./prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-export default auth((req) => {
-  const user = req.auth?.user as User;
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  const user: User = session?.user as User;
+
+  if (!session) {
+    return NextResponse.rewrite(new URL("/not-found", request.url));
+  }
+
   if (
     (user.role as UserRole) !== UserRole.ADMIN &&
     user.role !== UserRole.SUPERADMIN
   ) {
-    return Response.redirect(new URL("/", req.url));
+    return NextResponse.rewrite(new URL("/not-found", request.url));
   }
-});
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/admin", "/admin/:path*"],
