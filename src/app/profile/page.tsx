981f4fde32f00010685a08efb2455c18d4961eef
banahaker <pn0818x@outlook.com>
2025-05-07 16:38:40 +0800

    feat: add profile page with authentication check and user greeting

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/lib/auth/auth";
+import { User } from "@/prisma/client";
+
+export default async function Profile() {
+  const session = await auth();
+  // @ts-ignore
+  const user: User | null = session?.user || null;
+
+  if (!user) {
+    return (
+      <div className="w-full h-screen flex flex-col items-center justify-center">
+        <p className="text-lg text-gray-500">
+          Please log in to view your profile.
+        </p>
+        <a
+          href="/api/auth/signin"
+          className="mt-4 px-4 py-2 bg-mingdao-blue text-white rounded border-mingdao-blue border  hover:bg-transparent hover:text-mingdao-blue transition duration-300 ease-in-out"
+        >
+          Log In
+        </a>
+      </div>
+    );
+  }
+
+  return <>Hello, welcome to your profile! Enjoy your stay!</>;
+}
