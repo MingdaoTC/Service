564b62e308e5b5397f47d587d30b892f8cd46f26
banahaker <pn0818x@outlook.com>
2025-05-13 09:39:27 +0800

    fix: rwd availability of profile page

--- src/app/profile/_profile/BasicInfo.tsx
+++ src/app/profile/_profile/BasicInfo.tsx
@@ -2,15 +2,18 @@ import { User } from "@/prisma/client";
 
 export default function BasicInfo({ user }: { user: User }) {
   return (
-    <div className="flex gap-3 items-center justify-start px-6 py-4 w-[90dvw] max-w-[1440px]  bg-white rounded-lg border border-mingdao-blue mx-auto mt-10">
+    <div className="flex gap-3 items-center justify-start px-6 py-4 w-[90dvw] max-w-[1440px]  bg-white rounded-lg border border-mingdao-blue mx-auto mt-10 max-lg:px-3 max-lg:gap-2">
       <img
         src={user.avatarUrl || "/default-avatar.png"}
         alt="User Avatar"
-        className="w-24 h-24 rounded-full"
+        className="w-24 h-24 rounded-full max-lg:w-20 max-lg:h-20"
       />
       <div className="flex flex-col gap-3 items-start justify-around px-4 cursor-default">
-        <p className="text-3xl">Good Morning, {user.displayName}</p>
-        <p className="text-sm text-mingdao-blue-dark font-thin bg-mingdao-blue-light rounded-full px-4 pt-1 pb-2">
+        <p className="text-3xl max-md:hidden">
+          Good Morning, {user.displayName}
+        </p>
+        <p className="text-2xl hidden max-md:block">{user.displayName}</p>
+        <p className="text-sm text-mingdao-blue-dark font-thin bg-mingdao-blue-light rounded-full px-4 pt-1 pb-2 max-lg:text-xs max-lg:px-2">
           @{user.username}
         </p>
       </div>
