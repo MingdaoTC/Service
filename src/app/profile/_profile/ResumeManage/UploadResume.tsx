1bb1c68f55d776ec57d76336accc61ebc82b627b
banahaker <pn0818x@outlook.com>
2025-05-14 00:30:19 +0800

    feat: implement resume upload functionality with validation and user association
    but you cannot see your resumes now lol

--- src/app/profile/_profile/ResumeManage/UploadResume.tsx
+++ src/app/profile/_profile/ResumeManage/UploadResume.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { useRef, useState } from "react";
+import { createNewResume } from "../actions/createNewResume";
+import OperationInfoDialog from "@/components/Global/OperationInfoDialog";
+
+import { resumeFileSizeLimit } from "../limitationConfig";
 
 export default function UploadResume() {
   const fileRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
   const [fileName, setFileName] = useState<string>("選擇檔案");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -14,12 +22,43 @@ export default function UploadResume() {
     }
   };
 
-  const handleNewResumeForm = () => {
-    return;
+  const handleNewResumeForm = async (formData: FormData) => {
+    setIsPending(true);
+    if (
+      !fileRef.current ||
+      !fileRef.current.files ||
+      !fileRef.current.files[0]
+    ) {
+      setErrorMessage("請選擇檔案");
+      return;
+    }
+
+    if (fileRef.current?.files[0].size > resumeFileSizeLimit) {
+      setErrorMessage("檔案大小不得超過 5MB");
+      setIsPending(false);
+      return;
+    }
+
+    const resume = await createNewResume(formData);
+
+    if (resume?.error) {
+      setErrorMessage(resume.error);
+      setIsPending(false);
+      return;
+    }
+
+    setIsPending(false);
+    setDialogMessage("上傳成功");
+    setIsDialogOpen(true);
   };
 
   return (
     <div className="flex flex-col items-start gap-4">
+      <OperationInfoDialog
+        isOpen={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        message={dialogMessage}
+      />
       <h1 className="text-3xl text-gray-900">管理你的履歷</h1>
       <form
         action={handleNewResumeForm}
@@ -27,6 +66,14 @@ export default function UploadResume() {
         className="flex flex-col p-4 gap-3 rounded-lg w-full bg-mingdao-blue-light"
       >
         <h2 className="text-xl text-mingdao-blue-dark font-bold">上傳新履歷</h2>
+        <span
+          className="text-red-500 transition-opacity duration-300 bg-red-200 py-1 px-4 rounded-full"
+          style={{
+            opacity: errorMessage ? 1 : 0,
+          }}
+        >
+          {errorMessage}
+        </span>
         <div className="flex flex-col md:flex-row gap-4 w-full">
           <div className="flex-1 flex flex-col gap-1">
             <label htmlFor="title" className="text-mingdao-blue-dark">
@@ -35,11 +82,13 @@ export default function UploadResume() {
             <input
               type="text"
               id="title"
+              name="title"
               className="border border-gray-300 rounded p-2 w-full"
+              required
             />
           </div>
           <div className="flex flex-col gap-1 md:w-44">
-            <p className="text-mingdao-blue-dark">上傳履歷</p>
+            <p className="text-mingdao-blue-dark">上傳履歷 (PDF)</p>
             <label
               htmlFor="file"
               className="block w-full px-4 py-2 bg-mingdao-blue text-white rounded cursor-pointer border-mingdao-blue border hover:bg-transparent hover:text-mingdao-blue transition duration-300 ease-in-out text-center whitespace-nowrap overflow-hidden text-ellipsis"
@@ -51,8 +100,11 @@ export default function UploadResume() {
               ref={fileRef}
               type="file"
               id="file"
+              name="resume"
               className="hidden"
               onChange={handleFileChange}
+              accept=".pdf"
+              required
             />
           </div>
         </div>
@@ -60,7 +112,7 @@ export default function UploadResume() {
           type="submit"
           className="px-4 py-2 bg-mingdao-blue text-white rounded border-mingdao-blue border hover:bg-transparent hover:text-mingdao-blue transition duration-300 ease-in-out"
         >
-          上傳
+          {isPending ? "上傳中..." : "上傳"}
         </button>
       </form>
     </div>
---
+++ src/app/profile/_profile/actions/createNewResume.ts
@@ -0,0 +1,72 @@
+"use server";
+import { upload } from "@/library/r2/upload";
+import { auth } from "@/library/auth";
+
+import { createResumeByUser } from "@/library/prisma/resume/create";
+
+import { User } from "next-auth";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { resumeFileSizeLimit } from "../limitationConfig";
+
+export async function createNewResume(formData: FormData) {
+  const session = await auth();
+  const user = session?.user as User;
+
+  if (!user || !user.email) {
+    return { error: "Unauthorized" };
+  }
+
+  try {
+    const file = formData.get("resume") as File;
+    const title = formData.get("title") as string;
+
+    if (!file || !(file instanceof File) || file.size === 0) {
+      return { error: "No file uploaded" };
+    }
+
+    if (file.type !== "application/pdf") {
+      return { error: "Only PDF files are allowed" };
+    }
+
+    if (file.size > resumeFileSizeLimit) {
+      return { error: "File size is too large" };
+    }
+
+    if (!title) {
+      return { error: "Title is required" };
+    }
+
+    const response = await uploadToBucket(
+      file,
+      `${user.email}/${title}_${Date.now()}.pdf`,
+    );
+
+    const _resume = await createResumeByUser(
+      {
+        name: title,
+        fileName: response.key as string,
+        fileUrl: response.url as string,
+      },
+      user.email,
+    );
+    revalidatePath("/profile");
+  } catch (error) {
+    console.error("Error uploading resume:", error);
+    return { error: "Failed to upload resume" };
+  }
+  redirect("/profile");
+}
+
+async function uploadToBucket(file: File | Blob, filename: string) {
+  try {
+    const response = await upload(
+      file,
+      `resume/${filename}`,
+      "application/pdf",
+    );
+    return response;
+  } catch (_) {
+    throw new Error("Failed to upload resume");
+  }
+}
