9a7e120526b74cf602101043815a7816a10f8161
banahaker <pn0818x@outlook.com>
2025-05-14 11:49:10 +0800

    feat: resume list

--- src/app/profile/_profile/ResumeManage/index.tsx
+++ src/app/profile/_profile/ResumeManage/index.tsx
@@ -1,9 +1,11 @@
 import UploadResume from "./UploadResume";
+import ResumeList from "./ResumeList";
 
 export default function ResumeManage() {
   return (
     <>
       <UploadResume />
+      <ResumeList />
     </>
   );
 }
